4b5044d8a960756d78ae53d01d0cd6eee727efaa
Kim Hovden Duong <kimhduo@gmail.com>
2017-03-10 15:38:12 +0100

    lool
    lol

---
+++ hello/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: Kim
+ */
+
+#include "main.h"
+
+main::main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+main::~main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ hello/src/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: Kim
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* MAIN_H_ */
