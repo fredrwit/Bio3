a453d3c30cdfa7166959f90456546b00ac179037
Kim Hovden Duong <kimhduo@gmail.com>
2017-03-10 15:35:36 +0100

    lol
    hei

--- hello/src/hello.cpp
+++ hello/src/hello.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Fuck you World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
