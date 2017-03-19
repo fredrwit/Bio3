1dde29b87c5395d7ff1671f7ec350da547832cda
Kim Hovden Duong <kimhduo@gmail.com>
2017-03-19 10:13:44 +0100

    nytt

--- testPrim/src/test2/Main.java
+++ testPrim/src/test2/Main.java
@@ -1,19 +1,13 @@
-package test;
+package test2;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.security.spec.EncodedKeySpec;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Random;
 
@@ -68,120 +62,12 @@ public class Main {
 		}
 	}
 	
-	public static void initUndirectedGraph(UndirectedGraph<Node> undirectedGraph, Graph graph) {
-		System.out.println("Initializing the undirected graph with edges...");
-		for (int i = 0; i < graph.nodes.length; i++) {
-			for (int j = 0; j < graph.nodes[i].length; j++) {
-				undirectedGraph.addNode(graph.nodes[i][j]);
-			}
-		}
-		for (int i = 0; i < graph.nodes.length; i++) {
-			for (int j = 0; j < graph.nodes[i].length; j++) {
-				for (int k = 0; k < graph.nodes[i][j].neighbors.size(); k++) {
-					Double length = distance(graph.nodes[i][j],graph.nodes[i][j].get_neighbor(k));
-					undirectedGraph.addEdge(graph.nodes[i][j], graph.nodes[i][j].get_neighbor(k), length);
-				}
-			}
-		}
-		
-	}
-	
 	public static double distance(Node one, Node two) {
         double square = Math.pow(one.rgb_values[0] - two.rgb_values[0], 2) 
             + Math.pow(one.rgb_values[1] - two.rgb_values[1], 2) 
             + Math.pow(one.rgb_values[2] - two.rgb_values[2], 2);
         return Math.pow(square, 0.5);
     }
-	
-	public static ArrayList<Chromosome> initialPopulation(UndirectedGraph<Node> graph, Graph bgraph, int size) {
-		System.out.println("Creating initial population...");
-		ArrayList<Chromosome> population = new ArrayList<Chromosome>();
-		Map<Double, List<Node>> edges = new HashMap<Double, List<Node>>();
-		List<List<Double>> edgeValues = new ArrayList<List<Double>>();
-		Iterator<Entry<Node, Map<Node, Double>>> iterator = graph.entryIterator();
-		Double id = 0.0;
-		while (iterator.hasNext()) {
-			Entry<Node, Map<Node, Double>> entry = iterator.next();
-			Entry<Node, Double> maxEntry = entry.getValue().entrySet().stream().max(Map.Entry.comparingByValue()).get();
-			edgeValues.add(new ArrayList<Double>(Arrays.asList(id,maxEntry.getValue())));
-			edges.put(id, Arrays.asList(entry.getKey(),maxEntry.getKey()));
-			id++;
-		}
-		Collections.sort(edgeValues, new Comparator<List<Double>>()
-		{
-		  public int compare(List<Double> o1, List<Double> o2)
-		  {
-		    return o2.get(1).compareTo(o1.get(1));
-		  }
-		});
-		
-		UndirectedGraph<Node> tempGraph;
-		
-		for (int i = 1; i < size+1; i++) {
-			tempGraph = new UndirectedGraph<Node>(graph);
-
-			int j = 1;
-			int removed = 0;
-			while (removed < i-1) {	
-				tempGraph.removeEdge(edges.get(edgeValues.get(j-1).get(0)).get(0),edges.get(edgeValues.get(j-1).get(0)).get(1));
-				if (edges.get(edgeValues.get(j-1).get(0)).get(0) == edges.get(edgeValues.get(j).get(0)).get(1) && edges.get(edgeValues.get(j-1).get(0)).get(1) == edges.get(edgeValues.get(j).get(0)).get(0)) {
-					j++;
-				}
-				removed++;
-				j++;
-				
-			}
-			
-			Chromosome newChrom = new Chromosome(graph.size());
-			
-//			for (int k = 1; k < graph.size()+1; k++) {
-//				Iterator<Entry<Node, Double>> newIterator = graph.edgesFrom(bgraph.getNode(k)).entrySet().iterator();
-//				while (newIterator.hasNext()) {
-//					
-//				}
-//				
-//				
-//				if (newChrom.getRepr()[k-1] == 0) {
-//					newChrom.update(k,bgraph.getNode(k).id);
-//						
-//					Iterator<Entry<Node, Double>> newIterator = graph.edgesFrom(bgraph.getNode(k)).entrySet().iterator();
-//					while (newIterator.hasNext()) {
-//						
-//					}
-//				}
-//				
-//				
-//			}
-			
-			Iterator<Entry<Node, Map<Node, Double>>> nodeIterator = tempGraph.entryIterator();
-			while (nodeIterator.hasNext()) {
-				Entry<Node, Map<Node, Double>> node = nodeIterator.next();
-				Iterator<Entry<Node, Double>> edgeIterator = node.getValue().entrySet().iterator();
-				if (node.getValue().size() > 0) {
-					while (edgeIterator.hasNext()) {
-						Entry<Node, Double> edge = edgeIterator.next();
-						if (newChrom.getRepr()[edge.getKey().id-1] == node.getKey().id) {
-							if (edgeIterator.hasNext()) {
-								continue;
-							}
-							else {
-								newChrom.update(node.getKey().id-1,node.getKey().id);
-							}
-						}
-						else {
-							newChrom.update(node.getKey().id-1,edge.getKey().id);
-						}
-					}
-				}
-				else {
-					newChrom.update(node.getKey().id-1,node.getKey().id);
-					
-				}
-			}
-			population.add(newChrom);
-		}
-		return population;
-	}
 	
 	public static int decode(Chromosome chrom, Graph graph) {
 		int currentCluster = 1;
@@ -286,9 +172,9 @@ public class Main {
 		
 	}
 	
-	public static ArrayList<Edge> prims(Graph graph) {
+	public static List<Edge> prims(Graph graph) {
         boolean[] added = new boolean[graph.getSize()];
-		ArrayList<Edge> mst = new ArrayList<>();
+		List<Edge> mst = new ArrayList<>();
         PriorityQueue<Edge> pq = new PriorityQueue<>((Object o1, Object o2) ->{
             Edge first = (Edge) o1;
             Edge second = (Edge) o2;
@@ -321,14 +207,6 @@ public class Main {
         return mst;
     }
 	
-	public static Chromosome encode(List<Edge> mst, Graph graph) {
-		Chromosome chrom = new Chromosome(graph.getSize());
-		for (Edge edge : mst) {
-			chrom.update(edge.startNode-1, edge.endNode);
-		}
-		return chrom;
-	}
-	
 	public static void initWeights(Graph graph) {
 		double weight;
 		for (int i = 0; i < graph.getSize(); i++) {
@@ -340,7 +218,7 @@ public class Main {
 		}
 	}
 	
-	public static Chromosome generateChromosome(ArrayList<Edge> mst, Graph graph){
+	public static Chromosome generateChromosome(List<Edge> mst, Graph graph){
         Chromosome chrom = new Chromosome(graph.getSize());
         for (Edge edge : mst) {
             if (chrom.getRepr()[edge.startNode-1] != 0){
@@ -357,55 +235,54 @@ public class Main {
         return chrom;
     }
 	
+	public static List<Chromosome> initPop (List<Edge> mst, Graph graph, int size) {
+		List<Chromosome> pop = new ArrayList<Chromosome>();
+		Collections.sort(mst, new Comparator<Edge>() {
+		    @Override
+		    public int compare(Edge o1, Edge o2) {
+		        if (o1.getWeight() > o2.getWeight()) {
+		        	return 1;
+		        }
+		        else if (o1.getWeight() < o2.getWeight()) {
+		        	return -1;
+		        }
+		        else {
+		        	return 0;
+		        }
+		    }
+		});
+		for (int i = 0; i < size; i++ ) {
+			ArrayList<Edge> tempMST = new ArrayList<Edge>(mst);
+			int removed = 0;
+			while (removed < i-1) {
+				mst.remove(0);
+				removed++;
+			}
+			pop.add(generateChromosome(tempMST, graph));
+		}
+		return pop;
+	}
+	
 	
 	public static void main(String[] args) {		
-		BufferedImage pixels = get_image("Test image/1/Test image.jpg");
+		BufferedImage pixels = get_image("Test image/yt.png");
 		Graph graph = new Graph(pixels.getHeight(),pixels.getWidth());
 		initGraph(graph,pixels);
 		initWeights(graph);
-		UndirectedGraph<Node> undirectedGraph = new UndirectedGraph<Node>();
-		initUndirectedGraph(undirectedGraph, graph);
-		UndirectedGraph<Node> mst2 = Prim.mst(undirectedGraph);
-		ArrayList<Edge> mst = prims(graph);
-		System.out.println(decode(generateChromosome(mst, graph), graph));
+		List<Edge> mst = prims(graph);
+		mst.remove(0);
+		mst.remove(0);
+		generateChromosome(mst, graph).printRepr();
+		//List<Chromosome> pop = initPop(mst, graph, 10);
 		
-//		double sum = 0;
-//		for (Edge edge : mst) {
-//			System.out.println(edge.startNode + " " + edge.endNode + " " + edge.getWeight());
-//			sum += edge.getWeight();
-//		}
-//		System.out.println(sum);
-//		System.out.println(mst.size());
-//		
-//		System.out.println(Arrays.toString(encode(mst, graph).getRepr()));
-		
-//		Iterator<Entry<Node, Map<Node, Double>>> lool = mst2.entryIterator();
-//		Double sum2 = 0.0;
-//		while (lool.hasNext()) {
-//			Entry<Node, Map<Node, Double>> entry = lool.next();
-//			sum2 += entry.getValue().values().stream().mapToDouble(i -> i).sum();
-//		}
-//		System.out.println(sum2/2);
-		
-		ArrayList<Chromosome> initPop = initialPopulation(mst2,graph, 1);
-	
-//		for (int z = 0; z < initPop.get(0).getRepr().length; z++) {
-//			if (initPop.get(2).getRepr()[z] != initPop.get(3).getRepr()[z]) {
-//				System.out.println("Success!");
-//			}
+//		for (int q = 0; q < pop.size(); q++) {
+//			System.out.println(decode(pop.get(q), graph));
+//			
 //		}
 		
-		for (int k = 0; k < initPop.size(); k++) {
-			
-//			System.out.println(Arrays.toString(initPop.get(k).getRepr()));
-			System.out.println(decode(initPop.get(k), graph));
-//			for (int a = 0; a < graph.getSize(); a++) {
-//				System.out.println("Node: " + (a+1) + " has segment: " + graph.getNode(a+1).getSegment());
-//			}
-			colorEdges(graph);
-			writeImage(graph, pixels, "loly", k);
-		}
-
+		colorEdges(graph);
+		writeImage(graph, pixels, "loly", 1);
+		
 		System.out.println("Done");
 
 	}
